836584e8babcc240697f529115f93a7cd696bd92
Laura Turk <laura.turk@cognizant.com>
2019-09-19 10:14:39 -0600

    git init with two projects

---
+++ HelloWorld3/src/Hello.java
@@ -0,0 +1,13 @@
+public class Hello {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+
+        int myFirstNumber = (10 + 5) + (2 * 10);
+        int mySecondNumber = 12;
+        int myThirdNumber = 13;
+        int myTotal = myFirstNumber + mySecondNumber + myThirdNumber;
+        int myLastOne = myTotal - 1000;
+        System.out.println(myLastOne);
+    }
+}
